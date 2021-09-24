109f11414827c07070347aa7e20c5b49ab056d6b
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-24 12:08:08 +0600

    display some product data

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Product.css';
 
-const Product = () => {
+const Product = (props) => {
+    console.log(props.product);
+    const {name,img,seller,price,stock} = props.product;
     return (
-        <div>
-            <h2>This is Product</h2>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <p><small>by:{seller}</small></p>
+                <p>Price:{price}$</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+            </div>
         </div>
     );
 };
