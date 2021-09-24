7c410f9ceb1ae9487683db210904bc510fad268d
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-24 11:50:06 +0600

    display bangla system data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
 
 const Shop = () => {
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        fetch('./products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
     return (
-        <div>
-            <h2>This is Shop</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                <h2>Products:{products.length}</h2>
+                {
+                    products.map(product => console.log(product))
+                }
+            </div>
+            <div className="cart-container">
+                <h3>Order summary</h3>
+                <h5>Items ordered:</h5>
+            </div>
         </div>
     );
 };
