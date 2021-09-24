3ded25e59a2c6d7bce5618d15974f7e2d3b1545a
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-24 15:45:55 +0600

    display total price and click handler

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCart] =useState([]);
 
     useEffect(()=>{
         fetch('./products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+
+    const handleAddToCart = product=>{
+        const newCart =[...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(product => <Product product={product} key={product.key}></Product>)
+                    products.map(product => <Product product={product} key={product.key} handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h3>Order summary</h3>
-                <h5>Items ordered:</h5>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
